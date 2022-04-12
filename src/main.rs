8f2a2603752437654136da870b41504f74c2a85a
arfrie22 <arfrie22@students.shrewsbury.k12.ma.us>
2022-04-12 15:38:30 +0000

    The start of the suffering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
+    let connection = sqlite::open("file:data/mc_donalds.db").unwrap();
+
+    connection
+    .execute(
+        
+    )
+    .unwrap();
     println!("Hello, world!");
 }
