76bf66bcafbda6a1bd4244123a18f990d6392a90
arfrie22 <arfrie22@students.shrewsbury.k12.ma.us>
2022-04-13 15:07:22 +0000

    read all store ids in vec

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use sqlite::State;
+
 fn main() {
     let connection = sqlite::open("file:data/mc_donalds.db").unwrap();
 
-    connection
-    .execute(
-        
-    )
-    .unwrap();
-    println!("Hello, world!");
+    let mut stores = Vec::new();
+
+    let mut statement = connection
+        .prepare("select identifier_value from us where sub_division=\"MA\"")
+        .unwrap();
+
+    while let State::Row = statement.next().unwrap() {
+        stores.push(statement.read::<i64>(0).unwrap());
+    }
+
+    
 }
