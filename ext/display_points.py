0b7291c96376ba41df6ecc85412ebd2798d590e1
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2022-05-06 13:00:21 -0400

    updated display code to show best path and rendered greedy gif

--- ext/display_points.py
+++ ext/display_points.py
@@ -1,24 +1,79 @@
-import sqlite3
-from matplotlib import image
+import pandas as pd
+import pyarrow
+import matplotlib as mpl
 from matplotlib import pyplot as plt
-  
-data = image.imread('../data/world.200412.3x5400x2700.jpg')
-con = sqlite3.connect('../data/mc_donalds.db')
-cur = con.cursor()
-cur.execute('select * from us')
+import numpy as np
+import PIL
+import imageio
+import os
+import tqdm
 
-lats = []
-longs = []
 
-for result in cur:
-    lats.append(result[1])
-    longs.append(result[2])
+stores = pd.read_parquet('data/stores.csv')
+paths = pd.read_parquet('data/greedy.csv')
 
-    x = ((result[2] + 180) / 360) * 5400
-    y = ((90 - result[1]) / 180) * 2700
-    plt.plot(x, y, marker='v', color="red")
+os.makedirs('frames', exist_ok=True)  
+data = mpl.image.imread('data/world.200412.3x5400x2700.jpg')
+# PIL.Image.MAX_IMAGE_PIXELS = None
+# data = image.imread('data/Full Map.jpg')
+height = len(data)
+width = len(data[0])
+
+plt.figure(figsize=(width/100, height/100))
+
+stores['x'] = ((stores.to_numpy()[:,2] + 180) / 360) * width
+stores['y'] = ((90 - stores.to_numpy()[:,1]) / 180) * height 
+points = np.array(stores.to_numpy())
+
+for point in points:
+    plt.plot(point[3], point[4], marker='v', color="red")
 
 plt.imshow(data)
-plt.ylim([((90 - min(lats)) / 180) * 2700, ((90 - max(lats)) / 180) * 2700])
-plt.xlim([((min(longs) + 180) / 360) * 5400, ((max(longs) + 180) / 360) * 5400])
-plt.show()
+plt.ylim([points[:, 4].min(), points[:, 4].max()])
+plt.xlim([points[:, 3].min(), points[:, 3].max()])
+
+filenames = []
+shorest_path_length = 0
+shorest_path = None
+i = 0
+for path in tqdm.tqdm(paths.to_numpy()[0:20]):
+    lines = []
+    
+    x = []
+    y = []
+    
+    for store in path[0]:
+        x.append(stores[stores['store_id'] == store]['x'])
+        y.append(stores[stores['store_id'] == store]['y'])
+    
+    if path[1] < shorest_path_length or shorest_path == None:
+            shorest_path = [x, y]
+            shorest_path_length = path[1]
+
+    lines.extend(
+        plt.plot(x, y, color = 'blue', linewidth=3, linestyle='-.')
+    )
+
+    lines.extend(
+        plt.plot(shorest_path[0], shorest_path[1], color = 'green', linewidth=1, linestyle='-.')
+    )
+
+    # create file name and append it to a list
+    filename = f'frames/{i}.png'
+    filenames.append(filename)
+    i += 1
+    
+    # save frame
+    plt.savefig(filename, dpi=100)
+
+    for line in lines:
+        line.remove()
+# build gif
+with imageio.get_writer('mygif.gif', mode='I') as writer:
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+        
+# Remove files
+for filename in set(filenames):
+    os.remove(filename)
