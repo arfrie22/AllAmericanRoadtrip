2c345bfbc0593a5143570fca6c332b4b9f803563
Andrew Friedman <arfrie22@students.shrewsbury.k12.ma.us>
2022-05-19 22:10:16 -0400

    further start of aco, most likely will not finish

--- tsp_aco.py
+++ tsp_aco.py
@@ -6,12 +6,19 @@ import math
 import os
 from rich.progress import Progress
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import numpy as np
 
 ITERATIONS = 1000
-ANTS = 10000
+ANTS = 100
+DISTANCE_POWER = 4
+PHEROMONE_POWER = 1
+PHEROMONE_INTENSITY = 3
+PHEROMONE_INTENSITY_POWER = 10
+INITIAL_PHEROMONE_INTENSITY = 1
+EVAPORATION_RATE = 0.3
 
 
-def calculate_length(store):
+def simulate_ant(ant):
     # length = 0
     # stores_used = []
     # last_store = store
@@ -22,25 +29,41 @@ def calculate_length(store):
     #     stores_used.append(last_store)
     #     length += targets.iloc[0]['distance']
     # return [stores_used, length]
-    return ""
+    length = ant+1
+    path = [1, 2, 3, 45]
+    return ant, length, path
+    # return ""
 
 distances = pd.read_parquet('data/haversine.csv')
-stores = pd.read_parquet('data/stores.csv').to_numpy()[:,0]
+stores = pd.read_parquet('data/stores.csv').to_numpy()[:, 0]
 
-paths = []
+pheromone_trails = np.ones((len(stores), len(stores))) * INITIAL_PHEROMONE_INTENSITY
 
 with Progress() as progress:
     iterations_bar = progress.add_task("[red]Iterations", total=ITERATIONS)
     ants_bar = progress.add_task("[green]Ants", total=ANTS)
     for i in range(ITERATIONS):
-        # with ThreadPoolExecutor(max_workers=len(stores)) as ex:
-        with ThreadPoolExecutor(max_workers=ANTS) as ex:
-            futures = [ex.submit(calculate_length, store) for store in stores]
+        paths = pd.DataFrame(index=np.arange(ANTS), columns=['length', 'path'])
+        # with ThreadPoolExecutor(max_workers=ANTS) as ex:
+        with ThreadPoolExecutor(max_workers=10) as ex:
+            futures = [ex.submit(simulate_ant, ant) for ant in range(ANTS)]
             for future in as_completed(futures):
-                paths.append(future.result())
+                paths.iloc[future.result()[0]]['length'] = future.result()[1]
+                paths.iloc[future.result()[0]]['path'] = future.result()[2]
                 progress.update(ants_bar, advance=1)
         progress.reset(ants_bar)
         progress.update(iterations_bar, advance=1)
+        pheromone_trails -= EVAPORATION_RATE
+        shortest_path = paths.to_numpy()[:, 0].min()
+        for index, path in paths.iterrows():
+            strength = pow((shortest_path / path["length"]), PHEROMONE_INTENSITY_POWER) * PHEROMONE_INTENSITY
+            for node in range(len(path) - 1):
+                pheromone_trails[path[node], path[node+1]] += strength
+                pheromone_trails[path[node+1], path[node]] += strength
+
+            pheromone_trails[path[0], path[-1]] += strength
+            pheromone_trails[path[-1], path[0]] += strength
+
 
     
 
